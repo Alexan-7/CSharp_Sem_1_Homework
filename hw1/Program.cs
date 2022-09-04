74bd4f438bb645a356c4c18ea48f2606a3328b65
Sasha <a40546b@gmail.com>
2022-09-04 10:02:55 +0300

    first commit

---
+++ hw1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число и нажмите Enter");
+var n1 = Decimal.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число и нажмите Enter");
+var n2 = Decimal.Parse(Console.ReadLine());
+if (n1 > n2)
+    Console.WriteLine("Большее из этих чисел " + n1 + ", меньшее из этих чисел " + n2);
+else
+    Console.WriteLine("Большее из этих чисел " + n2 + ", меньшее из этих чисел " + n1);
---
+++ hw2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число и нажмите Enter");
+var n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число и нажмите Enter");
+var n2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите первое число и нажмите Enter");
+var n3 = int.Parse(Console.ReadLine());
+
+int max = n1;
+
+if (n1 > max) max = n1;
+if (n2 > max) max = n2;
+if (n3 > max) max = n3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
